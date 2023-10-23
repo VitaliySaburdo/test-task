b1eda99c4f54b166256e2de4837183ca9d14eff5
VitaliySaburdo <vsaburdo@gmail.com>
2023-10-23 22:47:13 +0300

    db

---
+++ db/usersDb.ts
@@ -0,0 +1,34 @@
+export const users = [
+  {
+    id: "1",
+    name: "John",
+    email: "John@mail.com",
+    birthday_date: "01-02-2001",
+    phone_number: "291873217",
+    address: "Kyiv",
+    },
+      {
+    id: "2",
+    name: "Bill",
+    email: "Bill@mail.com",
+    birthday_date: "01-02-2002",
+    phone_number: "291873214",
+    address: "Lviv",
+    },
+        {
+    id: "3",
+    name: "Michael",
+    email: "Michael@mail.com",
+    birthday_date: "01-02-2003",
+    phone_number: "291863217",
+    address: "Odessa",
+    },
+          {
+    id: "4",
+    name: "Ben",
+    email: "Ben@mail.com",
+    birthday_date: "01-02-2004",
+    phone_number: "291873213",
+    address: "Kharkiv",
+    },
+];
