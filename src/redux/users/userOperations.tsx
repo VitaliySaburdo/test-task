5d7c40e32f598d221fa4cae176261789c88d7767
VitaliySaburdo <vsaburdo@gmail.com>
2023-10-23 21:59:25 +0300

    add servises table

--- src/redux/users/userOperations.tsx
+++ src/redux/users/userOperations.tsx
@@ -17,12 +17,64 @@ export const getAllUsers = createAsyncThunk(
   }
 );
 
-// POST @ / addProduct
-export const addUser = createAsyncThunk(
-  'products/addProduct',
-  async (productData: string, thunkAPI) => {
+// GET @ / getUserById
+export const getUserById= createAsyncThunk(
+  'users/getUserById',
+  async (id: string, thunkAPI) => {
     try {
-      const res = await axios.post(`${BASE_URL}/table/`, productData);
+      const res = await axios.get(`/table/${id}`);
+      return res.data;
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+// POST @ / addUser
+export const addUser = createAsyncThunk(
+  'users/addUser',
+  async (userData: string, thunkAPI) => {
+    try {
+      const res = await axios.post(`${BASE_URL}/table/`, userData);
+      return res.data;
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+// PUT @ / changeUser
+export const changeUser = createAsyncThunk(
+  'users/changeUser',
+  async (id: string, thunkAPI) => {
+    try {
+      const res = await axios.put(`/table/${id}`);
+      return res.data;
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+// PATCH @ / changeUser
+export const changeFieldUser = createAsyncThunk(
+  'users/changeFieldUser',
+  async (id: string, thunkAPI) => {
+    try {
+      const res = await axios.patch(`/table/${id}`);
+      return res.data;
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+// DELETE @ / deleteProduct
+export const deleteProduct = createAsyncThunk(
+  'users/deleteUser',
+  async (id: string, thunkAPI) => {
+    try {
+      const res = await axios.delete(`${BASE_URL}/products/${id}` );
       return res.data;
     } catch (error: any) {
       return thunkAPI.rejectWithValue(error.message);
