ab7b82ead6e0e22fd8a1e3402323f58a501ba20f
VitaliySaburdo <vsaburdo@gmail.com>
2023-10-24 20:53:41 +0300

    fix route

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -11,17 +11,16 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Routes>
-        {/* <Route
+        <Route
           index
           element={
             <RestrictedRoute redirectTo="/tables" component={LoginPage} />
           }
-        /> */}
-        {/* <Route
+        />
+        <Route
           path="/tables"
           element={<PrivateRoute redirectTo="/" component={TablesPage} />}
-        /> */}
-        <Route index element={<TablesPage />} />
+        />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </ThemeProvider>
