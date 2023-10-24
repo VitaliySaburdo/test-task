b60b6361a8795f5532231ba35a021ed968ecfb30
VitaliySaburdo <vsaburdo@gmail.com>
2023-10-24 22:36:40 +0300

    finish

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,29 +1,25 @@
 import { Route, Routes } from "react-router-dom";
-// import { LoginPage } from "../../pages/LoginPage";
+import { LoginPage } from "../../pages/LoginPage";
 import { ThemeProvider } from "styled-components";
 import { theme } from "../../theme/theme";
 import { TablesPage } from "../../pages/TablesPage";
-// import { PrivateRoute } from "../../helpers/PrivateRoute";
-// import { RestrictedRoute } from "../../helpers/RestrictedRoute";
+import { PrivateRoute } from "../../helpers/PrivateRoute";
+import { RestrictedRoute } from "../../helpers/RestrictedRoute";
 import { NotFound } from "../../pages/NotFound";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Routes>
-        {/* <Route
+        <Route
           index
           element={
             <RestrictedRoute redirectTo="/tables" component={LoginPage} />
           }
-        /> */}
-        {/* <Route
+        />
+        <Route
           path="/tables"
           element={<PrivateRoute redirectTo="/" component={TablesPage} />}
-        /> */}
-        <Route
-          index
-          element={<TablesPage/>}
         />
         <Route path="*" element={<NotFound />} />
       </Routes>
